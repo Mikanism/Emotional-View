da92d1ed9a06d4e76fa3536d9e50dbad8fd8c68c
Mikanism <96637828+Mikanism@users.noreply.github.com>
2022-08-01 00:56:59 +0300

    Update InterFace.py

--- InterFace.py
+++ InterFace.py
@@ -1,8 +1,11 @@
+from email import header
+from fileinput import filename
 import streamlit as st
 import pandas as pd
 import datetime as dt
 import os
 import csv
+import io
 
 def timme(a):
     if a == 1:
@@ -115,10 +118,92 @@ with st.expander(""):
 def convert_df(df):
     return df.to_csv(header = head, index = False, sep = ';').encode('utf-8-sig')
 
-data1 = convert_df(pd.read_csv('ans1.csv',sep=';'))
-st.sidebar.download_button(
-    'Скачать',
-    data = data1,
-    file_name='ans1.csv',
-    on_click= df.to_csv('ans1.csv', encoding='utf-8-sig', header = head, index = False, sep = ';'))
+@st.cache
+def opros(data_ans, data):
+    for i in range(len(data_ans['date'])):
+        time_start = data_ans['date'][i][11:].split(':')
+        time_start = dt.timedelta(hours=int(time_start[0]), minutes=int(time_start[1]), seconds=int(time_start[2]))
+        time_end = data_ans['time'][i].split(':')
+        time_end = dt.timedelta(minutes=int(time_end[0]), seconds=int(time_end[1]))
+        ttime = time_start + time_end
+        if len(str(ttime)) == 7:
+                ttime = '0' + str(ttime) 
+        data_ans.loc[i,'time'] = data_ans['date'][i][:11] + ttime
 
+    data_ans.rename(columns={'time': 'date_end'}, inplace=True) 
+    #print(data_ans)
+
+    data.columns = ['TIME','Path']
+    for i in range(6):
+        data.insert(i+2,f'ans{i+1}','')
+    data.insert(8,'weight','')
+    data.insert(9,'opinion','')
+
+    sup_time = dt.timedelta(minutes = 0, seconds = 1)
+    for j in range(len(data_ans['date'])):
+        
+        while data_ans['date'][j] not in list(data['TIME']):
+            sp_time = data_ans['date'][j][11:].split(':')
+            sp_time = dt.timedelta(hours=int(sp_time[0]), minutes=int(sp_time[1]), seconds=int(sp_time[2]))
+            sp_time += sup_time
+
+            if len(str(sp_time)) == 7:
+                sp_time = '0' + str(sp_time) 
+                
+            data_ans.loc[j,'date'] = data_ans['date'][j][:11] + sp_time
+
+        for i in range(len(data['TIME'])):
+            try:
+                if data['TIME'][i] == data_ans['date'][j] and data['TIME'][i] != data['TIME'][i-1]:
+                    for m in range(6):
+                        data[f'ans{m+1}'][i] = data_ans[f'ans{m+1}'][j]
+                        data['weight'][i] = data_ans['weight'][j]
+                        data['opinion'][i] = data_ans['opinion'][j]
+                if data['TIME'][i] == data_ans['date_end'][j] and data['TIME'][i] != data['TIME'][i+1]:
+                    data['ans1'][i] = "END"
+            except KeyError:
+                if data['TIME'][i] == data_ans['date'][j]:
+                    for m in range(6):
+                        data[f'ans{m+1}'][i] = data_ans[f'ans{m+1}'][j]
+                        data['weight'][i] = data_ans['weight'][j]
+                        data['opinion'][i] = data_ans['opinion'][j]
+
+    #print(data)
+    head = data.columns
+    data.to_csv('file2.csv',encoding='utf-8-sig', header = head, index = False, sep = ';')
+    return data
+
+data1 = convert_df(pd.read_csv('ans1.csv',sep=';'))
+
+with st.sidebar:
+    tab1, tab2 = st.tabs(["1", "2"])
+    with tab1:
+        st.header('Для скачивания файла нажмите на кнопку')
+        st.download_button(
+            'Скачать',
+            data = data1,
+            file_name='ans1.csv',
+            on_click= df.to_csv('ans1.csv', encoding='utf-8-sig', header = head, index = False, sep = ';'))
+    
+    with tab2:
+        st.header('Для совмещения файлов выерете 2 файла и нажмите на кнопку')
+        uploaded_file = st.file_uploader("Выберите первый файл")
+        if uploaded_file is not None:
+
+            # Can be used wherever a "file-like" object is accepted:
+            data_ans = pd.read_csv(uploaded_file, sep = ";")
+            #st.write(data_ans)
+        
+        uploaded_file1 = st.file_uploader("Выберите второй файл")
+        if uploaded_file1 is not None:
+
+            # Can be used wherever a "file-like" object is accepted:
+            data = pd.read_csv(uploaded_file1, sep = ";", header = None)
+            #st.write(data)
+        
+        if uploaded_file is not None and uploaded_file1 is not None:
+            data2 = convert_df(opros(data_ans, data))
+            st.download_button(
+                'Совместить',
+                data = data2,
+                file_name = 'file2.csv')
